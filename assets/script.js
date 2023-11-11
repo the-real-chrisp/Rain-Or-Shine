7e7b20a77d14914451d6003247526803818a5a06
Chris Purcell <chrisp0000@gmail.com>
2023-11-11 14:05:46 -0800

    Added event listener to set user input to local storage as an array

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,10 @@
-var weatherInfo = document.getElementById('weatherInfo')
+const weatherInfo = document.getElementById('weatherInfo');
+const generateWeatherButton = document.getElementById('btn');
+
+generateWeatherButton.addEventListener("click", function () {
+    const city = document.getElementById('cityInput').value;
+    const cityList = !!localStorage.getItem('City') ? JSON.parse(localStorage.getItem('City')) : [];
+    cityList.unshift(city)
+    localStorage.setItem('City', JSON.stringify(cityList))
+});
+
