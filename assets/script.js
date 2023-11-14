d3604a174ad971de35deccbb3c079cb5785dbfa1
Chris Purcell <chrisp0000@gmail.com>
2023-11-13 16:23:50 -0800

    Built function to handle user input and convert to lat/long to pass through WeatherAPI

--- assets/script.js
+++ assets/script.js
@@ -6,5 +6,23 @@ generateWeatherButton.addEventListener("click", function () {
     const cityList = !!localStorage.getItem('City') ? JSON.parse(localStorage.getItem('City')) : [];
     cityList.unshift(city)
     localStorage.setItem('City', JSON.stringify(cityList))
+    
+    latLongConvert();
 });
 
+async function latLongConvert() {
+    const cityToConvert = await JSON.parse(localStorage.getItem('City'))[0];
+
+    const apiKey = "bfae21eab457fc78c9e3e8a208a66ee6";
+    const queryUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${cityToConvert},US&appid=${apiKey}`;
+
+    const response = await fetch(queryUrl);
+    const result = await response.json();
+    
+    localStorage.setItem('Lat', JSON.stringify(result[0].lat))
+    localStorage.setItem('Long', JSON.stringify(result[0].lon))
+}
+
+// async function getWeatherData {
+//     const response = await fetch("")
+// }
