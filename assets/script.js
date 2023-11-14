536bb9783fdb40ecde3bc0e519e7e219ada3bcaa
Chris Purcell <chrisp0000@gmail.com>
2023-11-13 16:44:34 -0800

    Built function to fet weather data from user input

--- assets/script.js
+++ assets/script.js
@@ -4,10 +4,11 @@ const generateWeatherButton = document.getElementById('btn');
 generateWeatherButton.addEventListener("click", function () {
     const city = document.getElementById('cityInput').value;
     const cityList = !!localStorage.getItem('City') ? JSON.parse(localStorage.getItem('City')) : [];
-    cityList.unshift(city)
-    localStorage.setItem('City', JSON.stringify(cityList))
+    cityList.unshift(city);
+    localStorage.setItem('City', JSON.stringify(cityList));
     
     latLongConvert();
+    getWeatherData();
 });
 
 async function latLongConvert() {
@@ -19,10 +20,20 @@ async function latLongConvert() {
     const response = await fetch(queryUrl);
     const result = await response.json();
     
-    localStorage.setItem('Lat', JSON.stringify(result[0].lat))
-    localStorage.setItem('Long', JSON.stringify(result[0].lon))
+    localStorage.setItem('Lat', JSON.stringify(result[0].lat));
+    localStorage.setItem('Lon', JSON.stringify(result[0].lon));
 }
 
-// async function getWeatherData {
-//     const response = await fetch("")
-// }
+async function getWeatherData() {
+    const cityLat = await JSON.parse(localStorage.getItem('Lat'));
+    const cityLon = await JSON.parse(localStorage.getItem('Lon'));
+    
+    const apiKey = "02c80f15f0dd6c40003962929b36a86c";
+    const queryUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${cityLat}&lon=${cityLon}&appid=${apiKey}`;
+
+    const response = await fetch(queryUrl);
+    const result = await response.json();
+
+    console.log(result)
+}
+
